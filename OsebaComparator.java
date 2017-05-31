57d55a6d51fbe92fa4d1f588ed6083b592ac6f11
Marko Žele <zele.marko5@gmail.com>
2017-05-31 20:43:54 +0200

    Create OsebaComparator.java

---
+++ OsebaComparator.java
@@ -0,0 +1,13 @@
+package test;
+
+import java.util.Comparator;
+
+public class OsebaComparator implements Comparator<Oseba>{
+
+	@Override
+	public int compare(Oseba o1, Oseba o2) {
+		// TODO Auto-generated method stub
+		return o1.getUsername().compareTo(o2.getUsername());
+	}
+	
+}
